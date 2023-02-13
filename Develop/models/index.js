75e589f68fdcc1519818e1d92a889d003356ebc3
zcord <zeus.cordeiro@gmail.com>
2023-02-13 13:23:33 -0500

    Fixed modal bugs. first GET route

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,23 +6,29 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'product_id'
+  foreignKey: 'category_id'
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id',
+  foreignKey: 'category_id',
   onDelete: 'CASCADE'
 })
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: 'ProductTag'
+  through: {
+    model: ProductTag,
+    unique: false
+  }
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: 'ProductTag'
+  through: {
+    model: ProductTag,
+    unique: false
+  }
 })
 
 module.exports = {
