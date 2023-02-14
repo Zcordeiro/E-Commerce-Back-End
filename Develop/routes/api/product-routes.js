c192ce165249f5d6ccf1ffac8d055672ad172517
zcord <zeus.cordeiro@gmail.com>
2023-02-14 11:05:29 -0500

    Started routes. Imported sequelize

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -13,13 +13,26 @@ router.get('/', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag }]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
